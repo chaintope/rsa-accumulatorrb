50122ef7d824c2b9a9f1912c80bfd6ce773696e5
azuchi <azuchi@chaintope.com>
2019-09-27 16:21:14 +0900

    Add spec for hash_to_prime

--- spec/rsa/acc/functions_spec.rb
+++ spec/rsa/acc/functions_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe RSA::ACC::Functions do
     it 'should convert element to prime' do
       p1 = functions.hash_to_prime("hoge")
       expect(OpenSSL::BN.new(p1).prime?).to be true
+      expect(functions.hash_to_prime("hoge")).to eq(p1) # same value outputs same prime number.
+
       p2 = functions.hash_to_prime("foge")
       expect(p1 == p2).to be false
     end
